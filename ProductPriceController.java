2adebd8aaa55778ab67afc96691fad3b1ca97116
srinadh-pasupuleti <69150721+srinadh-pasupuleti@users.noreply.github.com>
2020-09-02 14:13:36 +0530

    Add files via upload

---
+++ ProductPriceController.java
@@ -0,0 +1,27 @@
+package com.cts.product.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cts.product.dao.ProductPriceDao;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/price")
+public class ProductPriceController {
+	
+	@Autowired
+	private ProductPriceDao prodPrice;
+	
+	@GetMapping("/get/{id}")
+	public Double getPrice(@PathVariable int id) {
+		Double price=prodPrice.getPrice(id);
+		System.out.println("======> Getting price from Port 2222 and  Price is  "+price +" <======");
+		return prodPrice.getPrice(id);
+	}
+
+}
